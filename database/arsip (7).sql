79a6e8e17e5f20a0152019c77a703ccb677e9fc8
nurulvita <vitanurul242@gmail.com>
2024-04-16 17:59:53 +0800

    add-superadmin

--- database/arsip (7).sql
+++ database/arsip (7).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2024 at 03:03 PM
+-- Generation Time: Apr 16, 2024 at 11:59 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `arsip`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superadmin`
+--
+
+CREATE TABLE `superadmin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `superadmin`
+--
+
+INSERT INTO `superadmin` (`id`, `nama`, `email`, `username`, `password`, `role`) VALUES
+(1, 'superadmin', 'superadmin@gmail.com', 'superadmin', '202cb962ac59075b964b07152d234b70', 'superadmin');
 
 -- --------------------------------------------------------
 
@@ -45,15 +67,8 @@ CREATE TABLE `surat_keluar` (
 --
 
 INSERT INTO `surat_keluar` (`id`, `nomor`, `kategori`, `tujuan`, `perihal`, `tgl_keluar`, `waktu`, `file`, `status`, `keterangan`) VALUES
-(21, '003/SPm/INFORSA/II/2024', 'Peminjaman', 'Ketua Program Studi', 'Permohonan Pembuatan SK', '2024-03-21', '2024-03-29 17:44:00', '../../../pdf/pdfsk/290320241744005Spm-SuratProposalDanaOrtuINSAN2024.pdf', 'acc', ''),
-(22, '003/SPm/INFORSA/II/2024', 'Permohonan', 'Dekan Fakultas Teknik', 'Permohonan Pembuatan SK', '2024-03-15', '2024-03-29 18:43:00', '../../../pdf/pdfsk/290320241843UndanganDelegasiRoadshowKSEUNMUL - INFORSA.pdf', 'acc', ''),
-(24, '001/SPm/INFORSA/II/2024', 'Peminjaman', 'Ketua Program Studi', 'Undangan Buka Bersama', '2024-03-15', '2024-03-29 21:46:00', '../../../pdf/pdfsk/290320242146UndanganDelegasiRoadshowKSEUNMUL - INFORSA.pdf', 'acc', ''),
-(27, '003/SPm/INFORSA/II/2024', 'Mandat', 'Dekan Fakultas Teknik', 'Permohonan Pembuatan SK', '2024-03-08', '2024-03-29 21:26:00', '../../pdf/pdfsk/290320242126UndanganRakerHIMAPKOM.pdf', 'ditolak', 'penomoran salah'),
-(29, '002/SPm/INFORSA/II/2024', 'Pemberitahuan', 'Ketua Program Studi', 'Permohonan Pembuatan SK', '2024-03-12', '2024-03-29 21:48:00', '../../pdf/pdfsk/290320242148005Spm-SuratProposalDanaOrtuINSAN2024.pdf', 'ditolak', 'Tanggal salah'),
-(30, '001/SPm/INFORSA/II/2024', 'Peminjaman', 'Dekan Fakultas Teknik', 'Undangan Buka Bersama', '2024-03-15', '2024-03-29 21:48:00', '../../pdf/pdfsk/290320242148013SU-UndanganBukberKaprodi.pdf', 'acc', ''),
-(31, '003/SPm/INFORSA/II/2024', 'Pemberitahuan', 'Dekan Fakultas Teknik', 'Permohonan Pembuatan SK', '2024-03-23', '2024-03-30 15:01:00', '../../pdf/pdfsk/300320241458005Spm-SuratProposalDanaOrtuINSAN2024.pdf', 'acc', ''),
-(32, '001/SPm/INFORSA/II/2024', '', 'Ketua Program Studi', 'Permohonan Pembuatan SK', '2024-03-09', '2024-03-31 17:08:00', '../../../pdf/pdfsk/310320241708SPmSK.pdf', 'acc', ''),
-(33, '003/SPm/INFORSA/II/2024', '', 'Ketua Program Studi', 'Permohonan Pembuatan SK', '2024-03-21', '2024-03-31 17:12:00', '../../../pdf/pdfsk/310320241712SPmSK.pdf', 'acc', '');
+(34, '005Spm-Surat Proposal Dana Ortu INSAN 2024', 'Permohonan', 'Orang Tua Mahasiswa', 'Permohonan Bantuan Dana', '2024-03-04', '2024-04-02 17:53:00', '../../pdf/pdfsk/020420241753005Spm-SuratProposalDanaOrtuINSAN2024.pdf', 'acc', ''),
+(35, '006/005/SPp/DEPT-RELEKAT/INFORSA/III /2024', 'Peminjaman', 'Kepala Yayasan Jendela Langit Semesta', 'Permohonan Peminjaman Tempat', '2024-03-10', '2024-04-03 11:19:00', '../../pdf/pdfsk/030420241119006Spp_Permohonan_Peminjaman_Tempat.pdf', 'acc', '');
 
 -- --------------------------------------------------------
 
@@ -102,11 +117,18 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `nama`, `email`, `username`, `password`, `departemen`, `jabatan`, `foto`) VALUES
 (13, 'Nurul Vita Azizah', 'nurul@gmail.com', 'nurulvta', '202cb962ac59075b964b07152d234b70', 'bpi', 'sekretaris umum', 'uploads/IMG_20220801_083034_875.jpg'),
-(14, 'Nindy Pramudita', 'nindy@gmail.com', 'nindypr', '202cb962ac59075b964b07152d234b70', 'relekat', 'sekretaris departemen', 'uploads/IMG_20220703_213217_549.jpg');
+(14, 'Nindy Pramudita', 'nindy@gmail.com', 'nindypr', '202cb962ac59075b964b07152d234b70', 'relekat', 'sekretaris departemen', '../../../uploads/160420241135IMG_20220714_233658.jpg'),
+(21, 'Salsabila Naura', 'salsa@gmail.com', 'salsabr', '202cb962ac59075b964b07152d234b70', 'biro_inkref', 'sekretaris departemen', '../../../uploads/-6159212862195739996_121.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `surat_keluar`
@@ -131,10 +153,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `surat_keluar`
 --
 ALTER TABLE `surat_keluar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `surat_masuk`
@@ -146,7 +174,7 @@ ALTER TABLE `surat_masuk`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
